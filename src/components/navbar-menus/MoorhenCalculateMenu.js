059d57bee8477eb7221757e0c80ccff755ec2a87
Martin Noble <martinemnoble1@mac.com>
2023-05-22 11:17:04 +0100

    Fix for page overflow relating to Ketcher

--- src/components/navbar-menus/MoorhenCalculateMenu.js
+++ src/components/navbar-menus/MoorhenCalculateMenu.js
@@ -3,13 +3,11 @@ import { useState } from "react";
 import { MoorhenSuperposeMenuItem, MoorhenLoadScriptMenuItem } from "../menu-item/MoorhenMenuItem";
 import { MoorhenScriptModal } from "../modal/MoorhenScriptModal";
 import { MenuItem } from "@mui/material";
-import { KetcherModal } from "../modal/KetcherModal"
 
 export const MoorhenCalculateMenu = (props) => {
     const [popoverIsShown, setPopoverIsShown] = useState(false)
     const menuItemProps = { setPopoverIsShown, ...props }
     const [showCodeEditor, setShowCodeEditor] = useState(false)
-    const [showLigandBuilder, setShowLigandBuilder] = useState(false)
 
     return <>
         <NavDropdown
@@ -20,18 +18,14 @@ export const MoorhenCalculateMenu = (props) => {
             show={props.currentDropdownId === props.dropdownId}
             onToggle={() => { props.dropdownId !== props.currentDropdownId ? props.setCurrentDropdownId(props.dropdownId) : props.setCurrentDropdownId(-1) }}>
             <MoorhenSuperposeMenuItem key="superpose_structures" {...menuItemProps} />
-            <MenuItem onClick={() => setShowLigandBuilder(true)}>
-                Ligand builder...
-            </MenuItem>
             {props.allowScripting && 
             <>
                 <MoorhenLoadScriptMenuItem {...menuItemProps} />
-                <MenuItem onClick={() => { setShowCodeEditor(true) }}>Interactive scripting...</MenuItem>
+                <MenuItem id="interactive-scripting-menu-item" onClick={() => { setShowCodeEditor(true) }}>Interactive scripting...</MenuItem>
             </>
             }
             {props.extraCalculateMenuItems && props.extraCalculateMenuItems.map( menu => menu)}
         </NavDropdown>
-        <KetcherModal show={showLigandBuilder} setShow={setShowLigandBuilder} {...menuItemProps} />            
         <MoorhenScriptModal show={showCodeEditor} setShow={setShowCodeEditor} {...menuItemProps} />            
     </>
 }
